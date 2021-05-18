3587af1dd9c64fa5290a60dde30ae7b88c59e9f9
Viacheslav Vasin <viacheslav.vasin@gmail.com>
2021-05-18 14:09:07 -0500

    Start script added

--- app.js
+++ app.js
@@ -175,6 +175,7 @@ app.use((err, req, res, next) => {
 
 
 //ADDING LISTENER TO THE PORT 3000
-app.listen(3000, () => {
-    console.log('Serving on port 3000...');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}...`);
 });
